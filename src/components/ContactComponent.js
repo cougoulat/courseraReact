94b8f28843547090b269ae339aed0c744f5696be
arnaud cougoulat <cougoulat@gmail.com>
2022-01-09 23:18:58 +0100

    React Redux Forms Revisited

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,6 +1,6 @@
 import React,{ Component} from 'react';
 import { Row, BreadcrumbItem,Breadcrumb,Button , Label,  FormGroup, Col,  } from 'reactstrap';
-import {Control, LocalForm, Errors} from 'react-redux-form';
+import {Control, Form, Errors,actions} from 'react-redux-form';
 import { Link } from 'react-router-dom';
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -16,6 +16,9 @@ class Contact extends Component {
 	handleSubmit(values){
 		console.log("Current state "+JSON.stringify(values));
 		alert("Current state "+JSON.stringify(values));
+		        this.props.resetFeedbackForm();
+		alert("Current state "+JSON.stringify(values));
+
 	}
 
 	render(){
@@ -62,7 +65,8 @@ class Contact extends Component {
 						<h3>Send us Your Feedback</h3>
 					</div>
 					<div className="col-12 col-md-9">
-						<LocalForm onSubmit={(value) =>this.handleSubmit(value)}>
+						<Form model="feedback" onSubmit={(values) => this.handleSubmit(values)}>
+
 							<Row className="form-group">
 								<Label htmlFor="firstname" md={2}>First Name</Label>
 								<Col md={10}>
@@ -182,7 +186,7 @@ class Contact extends Component {
 									<Button color="primary" type='submit'> Send Your Feedback</Button>
 								</Col>
 							</FormGroup>
-						</LocalForm>
+						</Form>
 					</div>
 				</div>
 			</div>
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,20 +1,24 @@
 import {createStore,combineReducers,applyMiddleware} from 'redux';
+import {createForms} from 'react-redux-form';
 import {Leaders} from './leaders';
 import {Promotions} from './promotions';
 import {Dishes} from './dishes';
 import {Comments} from './comments';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
+import {InitialFeedback} from './form';
 
 
 export const ConfigureStore = ()=>{
 	const store = createStore(
-		combineReducers(	{
-			dishes : Dishes,
-			leaders : Leaders,
-			promotions : Promotions,
-			comments: Comments
-
+		combineReducers({
+			dishes: Dishes,
+			comments: Comments,
+			promotions: Promotions,
+			leaders: Leaders,
+			...createForms({
+				feedback: InitialFeedback
+			})
 		}),
 		applyMiddleware(thunk,  logger)
 	);
